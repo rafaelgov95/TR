c492c745bc8e2910f4a51df13988ac2573975b0c
Mr.Viana <rafaelgov95@gmail.com>
2017-09-07 15:28:59 -0400

    up

--- Python/Lista-0/Exercicio-9/Exemplo-9/App.py
+++ Python/Lista-0/Exercicio-9/Exemplo-9/App.py
@@ -2,5 +2,11 @@
 
 import ModulosPythons.Servicos
 
+# Exemplo -9
+
+# Objetivo: Esse exemplo tem como objetivo demostrar a criação de um modulo. Foi criado um modulo de serviços diversos que
+# contém Funções como Data Atual, Preço do Bitcoin atual , Preço do Real Atual e Preço do Dolar Atual atraves da internet.
+
+
 print  ModulosPythons.Servicos.agora() # Esse aqui e um teste
 print ("Valor do Bitcoin R$:'%.4f' " % ModulosPythons.Servicos.bitcoin())# Valor do bitcoin em uma funcao no modulo :D
