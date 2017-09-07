578ee4bb56e3e4531c53a7381f1c58ac93ccbf20
Mr.Viana <rafaelgov95@gmail.com>
2017-09-07 15:31:10 -0400

    up

--- Python/Lista-0/Exercicio-9/Exemplo-9/App.py
+++ Python/Lista-0/Exercicio-9/Exemplo-9/App.py
@@ -9,4 +9,4 @@ import ModulosPythons.Servicos
 
 
 print  ModulosPythons.Servicos.agora() # Esse aqui e um teste
-print ("Valor do Bitcoin R$:'%.4f' " % ModulosPythons.Servicos.bitcoin())# Valor do bitcoin em uma funcao no modulo :D
+print "Valor do Bitcoin R$:'%.4f' " % ModulosPythons.Servicos.bitcoin()# Valor do bitcoin em uma funcao no modulo :D
