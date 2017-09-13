277087d6701af4a01e79b915b8fb5b0eb07e6171
Mr.Viana <rafaelgov95@gmail.com>
2017-09-13 11:19:58 -0400

    up

--- Python/Lista-0/Exercicio-11/Arquivo/Arquivo.py
+++ Python/Lista-0/Exercicio-11/Arquivo/Arquivo.py
@@ -3,8 +3,9 @@
 def AcessarArquivo():
     arquivo = open('Numeros', 'r') # abre o documento Numeros
     NewList= '' # Cria uma variavel string vazia, para armazenar os numeros extraidos do documento
+    r =[]
     for line in arquivo: # for
          NewList+=(line.strip()) # metodo strip() serve para tirar o \n
+         r = map(int, NewList.split(';'))  # map realiza a conversão em int e split faz o parse dos numeros entregando em uma lista
     arquivo.close() # Fecha conexão
-    r = map(int,NewList.split(';')) # map realiza a conversão em int e split faz o parse dos numeros entregando em uma lista
     return r
--- Python/Lista-0/Exercicio-3/Calculadora.py
+++ Python/Lista-0/Exercicio-3/Calculadora.py
@@ -1,5 +1,5 @@
 # -*- coding: UTF-8 -*-
-#Autor: Rafael Viana
+# Autor: Rafael Viana
 
 def main():
     while(1):
@@ -17,13 +17,13 @@ def lerNumeros():
 def sevira (operacao,):
     lista = lerNumeros()
     if(operacao==1):
-        return lista[1] + lista[0]
+        return float(lista[1]) + float(lista[0])
     elif (operacao == 2):
-        return lista[1] - lista[0]
+        return float(lista[1]) - float(lista[0])
     elif (operacao == 3):
-        return lista[1] / lista[0]
+        return float(lista[1]) / float(lista[0])
     elif (operacao == 4):
-        return lista[1] * lista[0]
+        return float(lista[1]) * float(lista[0])
 
 main()
 
