bc44ff08a9a6755ec3a2366eedbb0d30db55a9e9
Mr.Viana <rafaelgov95@gmail.com>
2017-09-18 20:13:57 -0400

    up

--- Exercicios/Bluetooth/Scan.py
+++ Exercicios/Bluetooth/Scan.py
@@ -1,4 +1,6 @@
 from bluetooth import *
+server_sock=BluetoothSocket(RFCOMM )
+
 
 print "performing inquiry..."
 
@@ -8,3 +10,11 @@ print "found %d devices" % len(nearby_devices)
 
 for name, addr in nearby_devices:
      print " %s - %s" % (addr, name)
+
+port = 1
+bd_addr = "48:5A:B6:F5:70:94"
+
+sock=BluetoothSocket( RFCOMM )
+sock.connect((bd_addr, port))
+
+sock.send("hello!!")
